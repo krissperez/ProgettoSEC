6f03fb45a8fef230cb6111e1b84c633870439642
krissperez <kriss23perez@gmail.com>
2024-06-19 15:25:06 +0200

    added deleted_at field

--- portaleSEC/src/Entity/Province.php
+++ portaleSEC/src/Entity/Province.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ProvinceRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProvinceRepository::class)]
@@ -15,6 +16,9 @@ class Province
 
     #[ORM\Column(length: 255)]
     private ?string $nome = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $deleted_at = null;
 
     public function getId(): ?int
     {
@@ -29,6 +33,18 @@ class Province
     public function setNome(string $nome): static
     {
         $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeInterface
+    {
+        return $this->deleted_at;
+    }
+
+    public function setDeletedAt(?\DateTimeInterface $deleted_at): static
+    {
+        $this->deleted_at = $deleted_at;
 
         return $this;
     }
